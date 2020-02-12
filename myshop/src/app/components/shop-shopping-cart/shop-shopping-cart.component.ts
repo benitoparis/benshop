980fffd4511bb9db0d19de3f872b64e4f2dab05c
benoit <benoit.gallou0gmail.com>
2020-02-12 23:38:06 +0100

    maj

--- myshop/src/app/components/shop-shopping-cart/shop-shopping-cart.component.ts
+++ myshop/src/app/components/shop-shopping-cart/shop-shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-shop-shopping-cart',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+   private router: Router,
+   private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
   }
